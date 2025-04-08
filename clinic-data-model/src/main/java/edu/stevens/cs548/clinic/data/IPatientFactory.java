241d6d051f8f4f0d5a1ffdbf39e13b4e915e6d70
Vidhi Ashok Palan <40350761+vidhipalan@users.noreply.github.com>
2025-04-07 23:45:02 -0400

    Delete clinic-data-model directory

--- clinic-data-model/src/main/java/edu/stevens/cs548/clinic/data/IPatientFactory.java
+++
@@ -1,7 +0,0 @@
-package edu.stevens.cs548.clinic.data;
-
-public interface IPatientFactory {
-
-	public Patient createPatient ();
-
-}
--- clinic-data-model/src/main/java/edu/stevens/cs548/clinic/data/IProviderFactory.java
+++
@@ -1,7 +0,0 @@
-package edu.stevens.cs548.clinic.data;
-
-public interface IProviderFactory {
-
-	public Provider createProvider ();
-
-}
